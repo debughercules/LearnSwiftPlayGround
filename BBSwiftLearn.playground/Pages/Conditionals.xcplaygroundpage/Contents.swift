3307640da5d7c2c94874ce31ff1ffcf014c2bb3b
Bharat Byan <bharat.byan@dell.com>
2022-11-14 01:31:00 +0530

    first commit

---
+++ BBSwiftLearn.playground/Pages/Conditionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+/*:
+ [< Previous](@previous)                    [Home](Introduction)                    [Next >](@next)
+ ## Conditonals
+ In this page, I will be showing how to loop through collections.
+ */
+
+import Foundation
+
+var greeting = "Hello, playground"
+
